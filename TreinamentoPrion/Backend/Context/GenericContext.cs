b3adf80bd1d798cfbd6abd52be64ee94ddd3fa4c
Cristiano Reis <cristiano_reis_nascimento@hotmail.com>
2020-10-13 08:56:32 -0300

    First Commit

---
+++ TreinamentoPrion/Backend/Context/GenericContext.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Context
+{
+    public class GenericContext : DbContext
+    {
+        public GenericContext(): base("Data Source=qa0087;Initial Catalog=Renewals;User Id=RenewalsUser;Password=RenewalsP@$$w0rd;")
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ TreinamentoPrion/Backend/Repositories/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using Backend.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repositories
+{
+    public class GenericRepository<T>:IDisposable where T : class
+    {
+        private GenericContext context;
+
+        public GenericRepository()
+        {
+            context = new GenericContext();
+        }
+
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
